dff048d69715021b91a6806c6910f2b07e5123c5
Doreen Chen <doreen.chen9@gmail.com>
2021-06-21 11:44:51 +0800

    add guess times

--- 46_guess_number.py
+++ 46_guess_number.py
@@ -1,14 +1,17 @@
-# 46. 猜數字遊戲
+# 46. 猜數字遊戲 & 47
 # 產生一個隨機整數 1~100
 # 讓使用者重複輸入數字去猜
 # 猜對的話 印出 "終於猜對了"
 # 猜錯的話 要告訴他 比答案大/小
+# 延伸 : 印出猜幾次
 
 import random  #亂數
 
 r = random.randint(1, 100)  # 產生亂數
+int_count = 0
 while True:
-	ans = int(input('猜猜數字是多少: '))
+    ans = int(input('猜猜數字是多少: '))
+    int_count += 1  # count = count +1
     if ans > r:
         print('比答案大')
     elif ans < r:
@@ -16,3 +19,4 @@ while True:
     else:
         print('終於猜對了')
         break
+    print('這是猜第 ', int_count, ' 次')
